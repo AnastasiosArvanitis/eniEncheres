8af4504548cdacaccee9057293fb7afd43f40eab
l_chiron <73356651+lchiron44@users.noreply.github.com>
2021-01-04 16:11:22 +0100

    SQL Script alimentation

---
+++ src/sql/ENI-Encheres-ScriptAlimentation-BDD.sql
@@ -0,0 +1,11 @@
+INSERT INTO CATEGORIES VALUES('Informatique');
+INSERT INTO CATEGORIES VALUES('Ameublement');
+INSERT INTO CATEGORIES VALUES('Vetement');
+INSERT INTO CATEGORIES VALUES('Sport & Loisirs');
+
+INSERT INTO UTILISATEURS VALUES('Alana001','Dupont','Alana','ada_eni@yopmail.com','0601020304','5 bis de l europe','44000','Nantes','abcd1234',0,0,1);
+INSERT INTO UTILISATEURS VALUES('Za','Pécuchès','Amaïa','zpa_eni@yopmail.com','0706020303','15 allee des taneurs','85000','La Roche Sur Yon','1234ABCD',5,0,1);
+INSERT INTO UTILISATEURS VALUES('Grugan','girardello','Hipolyte','ggh_eni@yopmail.com','0165653233','10 Rue Jean-claude Maisonneuve','44220','Couëron','AaBbCcDd',10,0,1);
+INSERT INTO UTILISATEURS VALUES('Luc_hel','Pugin-Bron-Lucchino','Hélène','lph_eni@yopmail.com','0236565339','350 Bis Boulevard dumoulin delagrange','29000','Brest','@123_mdp',15,0,1);
+INSERT INTO UTILISATEURS VALUES('Ant_pereira','viveiro-pereira','Marie-antoinette','avm_eni@yopmail.com','0601020308','6 rue yves montand','06000','Nice','aa-bb-cc-dd',20,0,1);
+INSERT INTO UTILISATEURS VALUES('PE_Coro','Coro','Paul emile','pcp_eni@yopmail.com','','66 Impasse de sembrumes','51290','Saint-Remy-en-Bouzemont-Saint-Genest-et-Isson','12345678',30,0,1);
