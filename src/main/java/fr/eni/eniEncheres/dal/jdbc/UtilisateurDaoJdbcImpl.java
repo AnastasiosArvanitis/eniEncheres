9b6a218eeccd14250afd561a06a210785a2dc97d
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-01-04 22:59:20 +0100

    adding enchere logger dans UtilisateurDaoJdbcImpl

--- src/main/java/fr/eni/eniEncheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
+++ src/main/java/fr/eni/eniEncheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
@@ -3,10 +3,14 @@ package fr.eni.eniEncheres.dal.jdbc;
 import fr.eni.eniEncheres.bo.Utilisateur;
 import fr.eni.eniEncheres.dal.DalException;
 import fr.eni.eniEncheres.dal.dao.UtilisateurDao;
+import fr.eni.eniEncheres.tools.EnchereLogger;
 
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class UtilisateurDaoJdbcImpl implements UtilisateurDao {
+
+    private Logger logger = EnchereLogger.getLogger("UtilisateurDaoJdbcImpl");
 
     @Override
     public void update(Utilisateur utilisateur) throws SQLException, DalException {
