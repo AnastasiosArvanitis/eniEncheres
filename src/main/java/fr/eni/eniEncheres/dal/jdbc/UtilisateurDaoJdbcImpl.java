82fbe31aca198884375ea589208d32c241dbbc9d
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-01-04 22:53:49 +0100

    adding enchere logger dans UtilisateurDaoJdbcImpl

--- src/main/java/fr/eni/eniEncheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
+++ src/main/java/fr/eni/eniEncheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
@@ -1,6 +1,12 @@
 package fr.eni.eniEncheres.dal.jdbc;
 
 import fr.eni.eniEncheres.dal.dao.UtilisateurDao;
+import fr.eni.eniEncheres.tools.EnchereLogger;
+
+import java.util.logging.Logger;
 
 public class UtilisateurDaoJdbcImpl implements UtilisateurDao {
+
+    private Logger logger = EnchereLogger.getLogger("UtilisateurDaoJdbcImpl");
+
 }
