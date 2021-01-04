01a2357c3bd6b6a37079998c96b41b0be9b18b63
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2021-01-04 22:31:55 +0100

    Ajout des Methodes update et selectById dans les fichier UtilisateurDao et UtilisateurDaoJdbcImpl

--- src/main/java/fr/eni/eniEncheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
+++ src/main/java/fr/eni/eniEncheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
@@ -1,6 +1,21 @@
 package fr.eni.eniEncheres.dal.jdbc;
 
+import fr.eni.eniEncheres.bo.Utilisateur;
+import fr.eni.eniEncheres.dal.DalException;
 import fr.eni.eniEncheres.dal.dao.UtilisateurDao;
 
+import java.sql.SQLException;
+
 public class UtilisateurDaoJdbcImpl implements UtilisateurDao {
+
+    @Override
+    public void update(Utilisateur utilisateur) throws SQLException, DalException {
+
+    }
+
+    @Override
+    public Utilisateur selectById(int id) throws SQLException, DalException {
+        return null;
+    }
+
 }
