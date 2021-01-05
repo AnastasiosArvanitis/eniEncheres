1a53426fcb44f1dd1dd68fbd47e9f8984abdddef
l_chiron <73356651+lchiron44@users.noreply.github.com>
2021-01-05 15:10:06 +0100

    Ajout Insert Utilisateur

--- src/main/java/fr/eni/eniEncheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
+++ src/main/java/fr/eni/eniEncheres/dal/jdbc/UtilisateurDaoJdbcImpl.java
@@ -7,125 +7,145 @@ import fr.eni.eniEncheres.dal.dao.UtilisateurDao;
 import fr.eni.eniEncheres.dal.jdbcTools.JdbcConnection;
 import fr.eni.eniEncheres.tools.EnchereLogger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Logger;
 
 public class UtilisateurDaoJdbcImpl implements UtilisateurDao {
 
-    private Logger logger = EnchereLogger.getLogger("UtilisateurDaoJdbcImpl");
+  private Logger logger = EnchereLogger.getLogger("UtilisateurDaoJdbcImpl");
 
-    @Override
-    public Utilisateur selectById(int id) throws SQLException, DalException {
-        Utilisateur utilisateur = null;
-        final String SELECT_BY_ID = "SELECT * FROM UTILISATEURS WHERE id = ?";
+  @Override
+  public Utilisateur selectById(int id) throws SQLException, DalException {
+    Utilisateur utilisateur = null;
+    final String SELECT_BY_ID = "SELECT * FROM UTILISATEURS WHERE id = ?";
 
-        try(Connection connection = JdbcConnection.connect()){
+    try(Connection connection = JdbcConnection.connect()){
 
-            PreparedStatement requete = connection.prepareStatement(SELECT_BY_ID);
-            requete.setInt(1, id);
-            ResultSet resultSet = requete.executeQuery();
+      PreparedStatement requete = connection.prepareStatement(SELECT_BY_ID);
+      requete.setInt(1, id);
+      ResultSet resultSet = requete.executeQuery();
 
-            if(resultSet.next()){
-                utilisateur = utilisateurBuilder(resultSet);
-            }
-        }catch(SQLException e){
-            logger.severe("Error method selectById " + e.getMessage() + "\n");
-            throw new DalException(e.getMessage(), e);
-        }
-        return utilisateur;
+      if(resultSet.next()){
+        utilisateur = utilisateurBuilder(resultSet);
+      }
+    }catch(SQLException e){
+      logger.severe("Error method selectById " + e.getMessage() + "\n");
+      throw new DalException(e.getMessage(), e);
+    }
+    return utilisateur;
+  }
+
+  @Override
+  public Utilisateur selectLogin(String pseudoOuEmail, String password) throws SQLException, DalException {
+    Utilisateur utilisateur = null;
+    final String sqlLogin = "select * from UTILISATEURS where (email=? or pseudo=?) and motDePasse=?";
+
+    try (Connection connection =JdbcConnection.connect()) {
+
+      PreparedStatement preparedStatement = connection.prepareStatement(sqlLogin);
+      preparedStatement.setString(1, pseudoOuEmail);
+      preparedStatement.setString(2, pseudoOuEmail);
+      preparedStatement.setString(3, password);
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      if (resultSet.next()) {
+        utilisateur = utilisateurBuilder(resultSet);
+      }
+    } catch (SQLException e) {
+      logger.severe("Error method selectLogin " + e.getMessage() + "\n");
+      throw new DalException(e.getMessage(), e);
     }
 
-    @Override
-    public Utilisateur selectLogin(String pseudoOuEmail, String password) throws SQLException, DalException {
-        Utilisateur utilisateur = null;
-        final String sqlLogin = "select * from UTILISATEURS where (email=? or pseudo=?) and motDePasse=?";
+    return utilisateur;
+  }
 
-        try (Connection connection =JdbcConnection.connect()) {
+  @Override
+  public Utilisateur update(Utilisateur utilisateur) throws SQLException, DalException {
+    final String UPDATE = "UPDATE UTILISATEURS SET pseudo = ?, nom = ?, prenom = ?, email = ?, telephone = ? , rue = ?, codePostal = ?, ville = ?, motDePasse = ? WHERE id = ?";
+    Utilisateur utilisateurRecupere = null;
+    try(Connection connection = JdbcConnection.connect()){
+      PreparedStatement requete = connection.prepareStatement(UPDATE);
+      requete.setString(1, utilisateur.getPseudo());
+      requete.setString(2, utilisateur.getNom());
+      requete.setString(3, utilisateur.getPrenom());
+      requete.setString(4, utilisateur.getEmail());
+      requete.setString(5, utilisateur.getTelephone());
+      requete.setString(6, utilisateur.getRue());
+      requete.setString(7, utilisateur.getCodePostal());
+      requete.setString(8, utilisateur.getVille());
+      requete.setString(9, utilisateur.getMotDePasse());
+      requete.setInt(10, utilisateur.getId());
+      requete.executeUpdate();
 
-            PreparedStatement preparedStatement = connection.prepareStatement(sqlLogin);
-            preparedStatement.setString(1, pseudoOuEmail);
-            preparedStatement.setString(2, pseudoOuEmail);
-            preparedStatement.setString(3, password);
-            ResultSet resultSet = preparedStatement.executeQuery();
+      utilisateurRecupere = FactoryDao.getUtilisateurDao().selectById(utilisateur.getId());
 
-            if (resultSet.next()) {
-                utilisateur = utilisateurBuilder(resultSet);
-            }
-        } catch (SQLException e) {
-            logger.severe("Error method selectLogin " + e.getMessage() + "\n");
-            throw new DalException(e.getMessage(), e);
-        }
-
-        return utilisateur;
+    }catch(SQLException e){
+      logger.severe("Error method update " + e.getMessage() + "\n");
+      throw  new DalException(e.getMessage(), e);
     }
+    return utilisateurRecupere;
+  }
 
-    @Override
-    public Utilisateur update(Utilisateur utilisateur) throws SQLException, DalException {
-        final String UPDATE = "UPDATE UTILISATEURS SET pseudo = ?, nom = ?, prenom = ?, email = ?, telephone = ? , rue = ?, codePostal = ?, ville = ?, motDePasse = ? WHERE id = ?";
-        Utilisateur utilisateurRecupere = null;
-        try(Connection connection = JdbcConnection.connect()){
-            PreparedStatement requete = connection.prepareStatement(UPDATE);
-            requete.setString(1, utilisateur.getPseudo());
-            requete.setString(2, utilisateur.getNom());
-            requete.setString(3, utilisateur.getPrenom());
-            requete.setString(4, utilisateur.getEmail());
-            requete.setString(5, utilisateur.getTelephone());
-            requete.setString(6, utilisateur.getRue());
-            requete.setString(7, utilisateur.getCodePostal());
-            requete.setString(8, utilisateur.getVille());
-            requete.setString(9, utilisateur.getMotDePasse());
-            requete.setInt(10, utilisateur.getId());
-            requete.executeUpdate();
+  @Override
+  public Utilisateur insert(Utilisateur ajoutUtilisateur) throws SQLException, DalException {
+    final String sqlInsert ="INSERT INTO UTILISATEURS (pseudo,nom,prenom,email,telephone,rue,codePostal,ville,motDePasse) VALUES (?,?,?,?,?,?,?,?,?)";
+    Utilisateur utilisateurCree = null;
+    int idAjout = 0;
+    try (Connection connection = JdbcConnection.connect()) {
+      PreparedStatement preparedStatement = connection.prepareStatement(sqlInsert, PreparedStatement.RETURN_GENERATED_KEYS);
+      preparedStatement.setString(1, ajoutUtilisateur.getPseudo());
+      preparedStatement.setString(2, ajoutUtilisateur.getNom());
+      preparedStatement.setString(3, ajoutUtilisateur.getPrenom());
+      preparedStatement.setString(4, ajoutUtilisateur.getEmail());
+      if (ajoutUtilisateur.getTelephone() == null) {
+        preparedStatement.setNull(5, Types.VARCHAR);
+      }
+      else
+      {
+        preparedStatement.setString(5, ajoutUtilisateur.getTelephone());
+      }
 
-            utilisateurRecupere = FactoryDao.getUtilisateurDao().selectById(utilisateur.getId());
+      preparedStatement.setString(6, ajoutUtilisateur.getRue());
+      preparedStatement.setString(7, ajoutUtilisateur.getCodePostal());
+      preparedStatement.setString(8, ajoutUtilisateur.getVille());
+      preparedStatement.setString(9, ajoutUtilisateur.getMotDePasse());
+      System.out.println(preparedStatement.toString());
+      preparedStatement.executeUpdate();
+      ResultSet resultSet = preparedStatement.getGeneratedKeys();
 
-        }catch(SQLException e){
-            logger.severe("Error method update " + e.getMessage() + "\n");
-            throw  new DalException(e.getMessage(), e);
-        }
-        return utilisateurRecupere;
+      if (resultSet.next()) {
+        idAjout = resultSet.getInt(1);
+        logger.info("Nouvel utilisateur crée avec l'id : " + idAjout + " "+ajoutUtilisateur.toString())  ;
+        utilisateurCree = selectById(idAjout);
+      }
     }
+    catch(SQLException e){
+      e.printStackTrace();
+      logger.severe("Erreur methode ajoutUtilisateur : " + ajoutUtilisateur.toString() );
 
-    @Override
-    public Utilisateur insert(Utilisateur utilisateur) throws SQLException, DalException {
-        return null;
     }
+    return utilisateurCree ;
+  }
 
-    private Utilisateur utilisateurBuilder(ResultSet rs) throws SQLException {
-        Utilisateur utilisateur = new Utilisateur();
-        utilisateur.setId(rs.getInt("id"));
-        utilisateur.setPseudo(rs.getString("pseudo"));
-        utilisateur.setNom(rs.getString("nom"));
-        utilisateur.setPrenom(rs.getString("prenom"));
-        utilisateur.setEmail(rs.getString("email"));
-        utilisateur.setTelephone(rs.getString("telephone"));
-        utilisateur.setRue(rs.getString("rue"));
-        utilisateur.setCodePostal(rs.getString("codePostal"));
-        utilisateur.setVille(rs.getString("ville"));
-        utilisateur.setMotDePasse(rs.getString("motDePasse"));
-        utilisateur.setCredit(rs.getInt("credit"));
-        utilisateur.setAdministration(rs.getByte("administrateur"));
-        utilisateur.setCompteActif(rs.getByte("compteActif"));
+  private Utilisateur utilisateurBuilder(ResultSet rs) throws SQLException {
+    Utilisateur utilisateur = new Utilisateur();
+    utilisateur.setId(rs.getInt("id"));
+    utilisateur.setPseudo(rs.getString("pseudo"));
+    utilisateur.setNom(rs.getString("nom"));
+    utilisateur.setPrenom(rs.getString("prenom"));
+    utilisateur.setEmail(rs.getString("email"));
+    utilisateur.setTelephone(rs.getString("telephone"));
+    utilisateur.setRue(rs.getString("rue"));
+    utilisateur.setCodePostal(rs.getString("codePostal"));
+    utilisateur.setVille(rs.getString("ville"));
+    utilisateur.setMotDePasse(rs.getString("motDePasse"));
+    utilisateur.setCredit(rs.getInt("credit"));
+    utilisateur.setAdministration(rs.getByte("administrateur"));
+    utilisateur.setCompteActif(rs.getByte("compteActif"));
 
-        return utilisateur;
-    }
+    return utilisateur;
+  }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
