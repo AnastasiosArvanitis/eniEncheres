52a04cf1bee7e35343d6612b1636fd2a85bcd799
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-01-05 22:18:16 +0100

    connection servlet et jsp

--- src/main/java/fr/eni/eniEncheres/servlets/utilisateur/Connection.java
+++ src/main/java/fr/eni/eniEncheres/servlets/utilisateur/Connection.java
@@ -1,17 +1,53 @@
 package fr.eni.eniEncheres.servlets.utilisateur;
 
+import fr.eni.eniEncheres.bll.BllException;
+import fr.eni.eniEncheres.bll.UtilisateurManager;
+import fr.eni.eniEncheres.bo.Utilisateur;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
 
 
 public class Connection extends HttpServlet {
+
+    private UtilisateurManager utilisateurManager = null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        utilisateurManager = UtilisateurManager.getInstance();
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        String pseudoOuEmail = request.getParameter("connection-identifiant");
+        String password = request.getParameter("password-identifiant");
+        Utilisateur utilisateur = null;
+        RequestDispatcher dispatcher = null;
+
+        try {
+            utilisateur = utilisateurManager.getUtilisateurLogin(pseudoOuEmail, password);
+            if(utilisateur == null) {
+                dispatcher = request.getRequestDispatcher("/WEB-INF/Pages/error.jsp?error=userNotExist");
+                dispatcher.forward(request, response);
+            } else {
+                HttpSession session = request.getSession();
+                session.setAttribute("utilisateur", utilisateur);
+
+                response.sendRedirect("/encheres/profile");
+            }
+        } catch (BllException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/webapp/WEB-INF/Pages/connection.jsp
+++ src/main/webapp/WEB-INF/Pages/connection.jsp
@@ -4,6 +4,7 @@
     <title>Connection</title>
     <style>
         <%@ include file="../../css/_global.css"%>
+        <%@ include file="../../css/connection.css"%>
     </style>
     <%--<link rel="stylesheet" type="text/css" href="./css/_global.css">--%>
 </head>
@@ -11,7 +12,23 @@
 <body>
 <%@ include file="./header.jsp"%>
 <main>
-    <h1>Connection Page</h1>
+    <div class="connection-container">
+        <h3>Connection Utilisateur</h3>
+        <form method="post" action="<%=request.getContextPath()%>/connection" class="connection-form">
+            <p><label for="connection-identifiant">Email ou pseudo:</label></p>
+            <p><input id="connection-identifiant" type="text" name="connection-identifiant"/></p>
+            <p><label for="connection-password">Mot de passe:</label></p>
+            <p><input id="connection-password" type="text" name="password-identifiant"/></p>
+            <p><input id="connection-submit" type="submit" value="submit"/></p>
+            <p></p>
+            <p>
+                <label for="connection-remember">Se souvenir de moi:
+                    <input id="connection-remember" type="checkbox" name="connection-remeber" value="connection-remeber"></label>
+            </p>
+            <p></p>
+            <p><a href="#">Mot de pass oublié</a></p>
+        </form>
+    </div>
 </main>
 
 <%@ include file="./footer.jsp"%>
