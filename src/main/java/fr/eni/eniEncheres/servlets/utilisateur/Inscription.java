6732754fdc37cfcfbdf2c136dbbce11982a8e51c
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-01-05 13:13:57 +0100

    modification ajout dispatcher

--- src/main/java/fr/eni/eniEncheres/servlets/utilisateur/Inscription.java
+++ src/main/java/fr/eni/eniEncheres/servlets/utilisateur/Inscription.java
@@ -5,15 +5,15 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "Inscription", value = "/Inscription")
 public class Inscription extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Pages/inscription.jsp");
+        dispatcher.forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 }
