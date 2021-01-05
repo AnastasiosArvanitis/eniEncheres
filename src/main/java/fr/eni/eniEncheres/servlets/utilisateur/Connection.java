6c997a0705fd113afe6e67e922267f46a3633013
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-01-05 13:13:08 +0100

    connection servlet et jsp

---
+++ src/main/java/fr/eni/eniEncheres/servlets/utilisateur/Connection.java
@@ -0,0 +1,21 @@
+package fr.eni.eniEncheres.servlets.utilisateur;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class Connection extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Pages/connection.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/Pages/connection.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Connection</title>
+</head>
+<body>
+<h1>Connection Page</h1>
+</body>
+</html>
