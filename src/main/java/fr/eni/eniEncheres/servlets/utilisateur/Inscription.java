4b5fcc2fbb839ca78e4220c1728f295e013e365d
l_chiron <73356651+lchiron44@users.noreply.github.com>
2021-01-05 23:35:24 +0100

    Ajout redirection connection lors de la création de compte + redirection avec attributs saisis en cas d'echec

--- src/main/java/fr/eni/eniEncheres/servlets/utilisateur/Inscription.java
+++ src/main/java/fr/eni/eniEncheres/servlets/utilisateur/Inscription.java
@@ -1,9 +1,14 @@
 package fr.eni.eniEncheres.servlets.utilisateur;
 
+import fr.eni.eniEncheres.bll.BllException;
+import fr.eni.eniEncheres.bll.UtilisateurManager;
+import fr.eni.eniEncheres.bo.Utilisateur;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Inscription extends HttpServlet {
     @Override
@@ -14,6 +19,43 @@ public class Inscription extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
-    }
+        String pseudo       = request.getParameter("pseudo");
+        String nom          = request.getParameter("nom");
+        String prenom       = request.getParameter("prenom");
+        String email        = request.getParameter("email");
+        String telephone    = request.getParameter("telephone");
+        String rue          = request.getParameter("rue");
+        String codePostal   = request.getParameter("codePostal");
+        String ville        = request.getParameter("ville");
+        String motDePasse   = request.getParameter("motDePasse");
+
+
+        Utilisateur ajoutUtilisateur = new Utilisateur(pseudo,nom,prenom,email,telephone,rue,codePostal,ville,motDePasse);
+        UtilisateurManager utilisateurManager = UtilisateurManager.getInstance();
+        Utilisateur utilisateur = null;
+        RequestDispatcher dispatcher = null;
+
+        try {
+            utilisateur = utilisateurManager.insert(ajoutUtilisateur);
+            System.out.println("insert réussi");
+            System.out.println(utilisateur.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if(utilisateur == null) {
+            request.setAttribute("ajoutUtilisateur", ajoutUtilisateur);
+            dispatcher = request.getRequestDispatcher("/WEB-INF/Pages/inscription.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            HttpSession session = request.getSession();
+            session.setAttribute("utilisateur", utilisateur);
+
+            response.sendRedirect("/encheres/profile");
+        }
+        }
+
+
+
+
 }
