b4ff78e2fead2d32ca80dcf606bf0f3e39e9b49c
l_chiron <73356651+lchiron44@users.noreply.github.com>
2021-01-05 19:02:56 +0100

    Ajout nouveau fichier Settings settingNews

---
+++ src/main/java/fr/eni/eniEncheres/dal/jdbcTools/SettingsNew.java
@@ -0,0 +1,32 @@
+package fr.eni.eniEncheres.dal.jdbcTools;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class SettingsNew {
+
+    private static Properties properties;
+    private static FileInputStream input;
+
+    static {
+        try {
+            properties = new Properties();
+
+            InputStream resourceAsStream = SettingsNew.class.getClassLoader().getResourceAsStream("settings.properties");
+            if (resourceAsStream != null) {
+                properties.load(resourceAsStream);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+        public static String getProperty(String key) {
+        String parametre = properties.getProperty(key, null);
+        return parametre;
+    }
+
+}
