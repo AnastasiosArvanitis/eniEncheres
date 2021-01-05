f51340fd0e96cc6b2bace41afbc9129a4ba7aa0d
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-01-05 11:12:01 +0100

    instance de UtilisateurDao

--- src/main/java/fr/eni/eniEncheres/dal/FactoryDao.java
+++ src/main/java/fr/eni/eniEncheres/dal/FactoryDao.java
@@ -1,4 +1,12 @@
 package fr.eni.eniEncheres.dal;
 
+import fr.eni.eniEncheres.dal.dao.UtilisateurDao;
+import fr.eni.eniEncheres.dal.jdbc.UtilisateurDaoJdbcImpl;
+
 public class FactoryDao {
+
+    public static UtilisateurDao getPostDao() {
+        return new UtilisateurDaoJdbcImpl();
+    }
+
 }
