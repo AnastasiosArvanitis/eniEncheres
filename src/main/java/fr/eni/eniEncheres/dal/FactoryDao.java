b42de4bd3391644fb87614d9f6910b1b24acf96d
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2021-01-06 19:27:11 +0100

    Ajout de la factory encheres

--- src/main/java/fr/eni/eniEncheres/dal/FactoryDao.java
+++ src/main/java/fr/eni/eniEncheres/dal/FactoryDao.java
@@ -1,13 +1,7 @@
 package fr.eni.eniEncheres.dal;
 
-import fr.eni.eniEncheres.dal.dao.ArticleDao;
-import fr.eni.eniEncheres.dal.dao.CategorieDao;
-import fr.eni.eniEncheres.dal.dao.RetraitDao;
-import fr.eni.eniEncheres.dal.dao.UtilisateurDao;
-import fr.eni.eniEncheres.dal.jdbc.ArticleDaoJdbcImpl;
-import fr.eni.eniEncheres.dal.jdbc.CategorieDaoJdbcImpl;
-import fr.eni.eniEncheres.dal.jdbc.RetraitDaoJdbcImpl;
-import fr.eni.eniEncheres.dal.jdbc.UtilisateurDaoJdbcImpl;
+import fr.eni.eniEncheres.dal.dao.*;
+import fr.eni.eniEncheres.dal.jdbc.*;
 
 public class FactoryDao {
 
@@ -20,4 +14,6 @@ public class FactoryDao {
     public static RetraitDao getRetraitDao() { return  new RetraitDaoJdbcImpl(); }
 
     public static ArticleDao getArticleDao() { return new ArticleDaoJdbcImpl(); }
-}
+
+    public static EnchereDao getEnchereDao(){ return  new EnchereDaoJdbcImpl(); }
+    }
