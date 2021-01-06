f52bb46009b4077809c84076aaa8cb56207d46df
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2021-01-06 18:44:24 +0100

    Ajout des methodes Categories

--- src/main/java/fr/eni/eniEncheres/dal/dao/CategorieDao.java
+++ src/main/java/fr/eni/eniEncheres/dal/dao/CategorieDao.java
@@ -4,9 +4,18 @@ import fr.eni.eniEncheres.bo.Categorie;
 import fr.eni.eniEncheres.dal.DalException;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface CategorieDao {
 
+    /* METHODE POUR L ADMINISTRATION */
+    Categorie insert(Categorie categorie) throws SQLException, DalException;
+    Categorie update(Categorie categorie) throws SQLException, DalException;
+    boolean delete(int id) throws SQLException, DalException;
+
+    /* METHODE IMPORTANTE */
+    List<Categorie> selectAllCategorie() throws SQLException, DalException;
     Categorie selectCategorieByID(int CategorieId) throws SQLException, DalException;
+    Categorie selectByName(String nomCategorie) throws SQLException, DalException;
 
 }
--- src/main/java/fr/eni/eniEncheres/dal/jdbc/CategorieDaoJdbcImpl.java
+++ src/main/java/fr/eni/eniEncheres/dal/jdbc/CategorieDaoJdbcImpl.java
@@ -10,6 +10,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class CategorieDaoJdbcImpl implements CategorieDao {
@@ -17,16 +19,34 @@ public class CategorieDaoJdbcImpl implements CategorieDao {
     private Logger logger = EnchereLogger.getLogger("CategorieDaoJdbcImpl");
 
     @Override
+    public List<Categorie> selectAllCategorie() throws SQLException, DalException {
+        List<Categorie> listCategorie = new ArrayList<>();
+        final String SELECT_ALL_CATEGORIE="SELECT * FROM CATEGORIES";
+        try(Connection connection = JdbcConnection.connect()){
+            PreparedStatement requete = connection.prepareStatement(SELECT_ALL_CATEGORIE);
+            ResultSet rs = requete.executeQuery();
+            while(rs.next()){
+                Categorie categorie = new Categorie();
+                categorie.setId(rs.getInt("id"));
+                categorie.setLibelle(rs.getString("libelle"));
+                listCategorie.add(categorie);
+            }
+        }catch (SQLException e){
+            logger.severe("Error method selectAllCategorie " + e.getMessage() + "\n");
+            throw new DalException(e.getMessage(), e);
+        }
+        return listCategorie;
+    }
+
+    @Override
     public Categorie selectCategorieByID(int CategorieId) throws SQLException, DalException {
         Categorie categorie = null;
         final String SELECT_BY_ID = "select * from CATEGORIES where id = ?";
-
         try {
             Connection connection = JdbcConnection.connect();
             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_BY_ID);
             preparedStatement.setInt(1, CategorieId);
             ResultSet resultSet = preparedStatement.executeQuery();
-
             if (resultSet.next()) {
                 categorie = categorieBuilder(resultSet);
             }
@@ -34,19 +54,49 @@ public class CategorieDaoJdbcImpl implements CategorieDao {
             logger.severe("Error method selectCategorieByID " + e.getMessage() + "\n");
             throw new DalException(e.getMessage(), e);
         }
-
         return categorie;
     }
 
+    @Override
+    public Categorie selectByName(String nomCategorie) throws SQLException, DalException {
+        Categorie categorie = null;
+        final String SELECT_BY_NAME="SELECT * FROM CATEGORIES WHERE libelle = ?";
+        try(Connection connection = JdbcConnection.connect()){
+            PreparedStatement requete = connection.prepareStatement(SELECT_BY_NAME);
+            requete.setString(1, nomCategorie);
+            ResultSet rs = requete.executeQuery();
+            if (rs.next()){
+                categorie = categorieBuilder(rs);
+            }
+        }catch (SQLException e){
+            logger.severe("Error method selectByName " + e.getMessage() + "\n");
+            throw new DalException(e.getMessage(), e);
+        }
+        return categorie;
+    }
 
     private Categorie categorieBuilder(ResultSet rs) throws SQLException {
         Categorie categorie = new Categorie();
         categorie.setId(rs.getInt("id"));
         categorie.setLibelle(rs.getString("libelle"));
-
         return categorie;
     }
 
+
+
+    /* METHODE POUR L ADMINISTRATION A FAIRE SI PROJET FINI*/
+    @Override
+    public Categorie insert(Categorie categorie) throws SQLException, DalException {
+        return null;
+    }
+    @Override
+    public Categorie update(Categorie categorie) throws SQLException, DalException {
+        return null;
+    }
+    @Override
+    public boolean delete(int id) throws SQLException, DalException {
+        return false;
+    }
 }
 
 
