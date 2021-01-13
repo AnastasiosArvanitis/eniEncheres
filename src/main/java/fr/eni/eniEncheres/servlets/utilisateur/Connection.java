b815bcf951064e377b049082274f5b850ea7df05
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2021-01-13 13:59:30 +0100

    ajout cookie pour retenir les identifiants de connection

--- src/main/java/fr/eni/eniEncheres/servlets/utilisateur/Connection.java
+++ src/main/java/fr/eni/eniEncheres/servlets/utilisateur/Connection.java
@@ -7,10 +7,7 @@ import fr.eni.eniEncheres.bo.Utilisateur;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -29,8 +26,19 @@ public class Connection extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String pseudoOuEmail = request.getParameter("connection-identifiant");
         String password = request.getParameter("password-identifiant");
+        String souvenir = "";
+        souvenir = request.getParameter("connection-remeber");
         Utilisateur utilisateur = null;
         RequestDispatcher dispatcher = null;
+
+        if(souvenir != null){
+            Cookie cookie = new Cookie("connection-identifiant", pseudoOuEmail);
+            cookie.setMaxAge(60 * 60 * 24 * 30);
+            response.addCookie(cookie);
+            Cookie cookiePass = new Cookie("password-identifiant", password);
+            cookiePass.setMaxAge(60 * 60 * 24 * 30);
+            response.addCookie(cookiePass);
+        }
 
         try {
             utilisateur = utilisateurManager.getUtilisateurLogin(pseudoOuEmail, password);
@@ -53,6 +61,18 @@ public class Connection extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("connection-identifiant")) {
+                    request.setAttribute("connection-identifiant", cookie.getValue());
+                }
+                if (cookie.getName().equals("password-identifiant")) {
+                    request.setAttribute("password-identifiant", cookie.getValue());
+                }
+
+            }
+        }
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Pages/connection.jsp");
         dispatcher.forward(request, response);
     }
--- src/main/java/fr/eni/eniEncheres/servlets/vente/AjoutVente.java
+++ src/main/java/fr/eni/eniEncheres/servlets/vente/AjoutVente.java
@@ -5,11 +5,8 @@ import fr.eni.eniEncheres.bo.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
+import javax.servlet.http.*;
+import java.io.*;
 
 
 import java.sql.SQLException;
